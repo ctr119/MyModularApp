ff6c801adadf1a8ef9f0f036c1455c65e4663d23
Victor Barrios <victor@beiningbogen.no>
2026-08-23 12:29:24 +0200

    Initial Commit

---
+++ MyModularApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyModularApp
+//
+//  Created by Victor Barrios Sanchez on 23/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyModularApp/MyModularAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyModularAppApp.swift
+//  MyModularApp
+//
+//  Created by Victor Barrios Sanchez on 23/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyModularAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
