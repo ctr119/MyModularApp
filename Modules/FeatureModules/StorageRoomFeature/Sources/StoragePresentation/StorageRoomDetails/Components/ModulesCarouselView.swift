28fbddb351b804cb0414f6a46e12531d5df90a94
Victor Barrios <victor@beiningbogen.no>
2026-08-31 23:33:51 +0200

    [refactor] Improve readability

--- Modules/FeatureModules/StorageRoomFeature/Sources/StoragePresentation/StorageRoomDetails/Components/ModulesCarouselView.swift
+++ Modules/FeatureModules/StorageRoomFeature/Sources/StoragePresentation/StorageRoomDetails/Components/ModulesCarouselView.swift
@@ -7,47 +7,55 @@ struct ModulesCarouselView: View {
 
     var body: some View {
         VStack(alignment: .leading) {
-            HStack {
-                Label {
-                    Text("Modules")
-                } icon: {
-                    Image(systemName: "shippingbox.fill")
-                        .foregroundStyle(.brown)
-                }
-
-                Spacer()
-
-                Button {
-                    didTapSeeAll()
-                } label: {
-                    HStack {
-                        Text("See all")
-                        Image(systemName: "chevron.right")
-                    }
-                }
-                .buttonStyle(.plain)
-            }
-            .monospaced()
-            .padding(.bottom, 6)
+            header
+                .padding(.bottom, 6)
 
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 6) {
                     ForEach(modules, id: \.id) { module in
-                        Button {
-
-                        } label: {
-                            Text(module.label)
-                                .monospaced()
-                                .padding(12)
-                                .padding(.horizontal, 6)
-                                .background(.black.opacity(0.1))
-                                .clipShape(Capsule())
-                        }
-                        .buttonStyle(.plain)
+                        carouselCell(module)
                     }
                 }
             }
         }
+    }
+
+    private var header: some View {
+        HStack {
+            Label {
+                Text("Modules")
+            } icon: {
+                Image(systemName: "shippingbox.fill")
+                    .foregroundStyle(.brown)
+            }
+
+            Spacer()
+
+            Button {
+                didTapSeeAll()
+            } label: {
+                HStack {
+                    Text("See all")
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .buttonStyle(.plain)
+        }
+        .monospaced()
+    }
+
+    private func carouselCell(_ module: Module) -> some View {
+        Button {
+
+        } label: {
+            Text(module.label)
+                .monospaced()
+                .padding(12)
+                .padding(.horizontal, 6)
+                .background(.black.opacity(0.1))
+                .clipShape(Capsule())
+        }
+        .buttonStyle(.plain)
     }
 }
 
