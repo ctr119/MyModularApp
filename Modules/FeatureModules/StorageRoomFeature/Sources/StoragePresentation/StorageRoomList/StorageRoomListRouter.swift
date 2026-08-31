2e5f8a55b8b498cc0766c5e075d0e3e9eaa2c958
Victor Barrios <victor@beiningbogen.no>
2026-08-31 23:22:54 +0200

    [feat] Add backwards navigation support to router

--- Modules/FeatureModules/StorageRoomFeature/Sources/StoragePresentation/StorageRoomList/StorageRoomListRouter.swift
+++ Modules/FeatureModules/StorageRoomFeature/Sources/StoragePresentation/StorageRoomList/StorageRoomListRouter.swift
@@ -1,23 +1,49 @@
 import SwiftUI
 import StorageDomain
 
+@MainActor
 @Observable
 class StorageRoomListRouter {
     enum Destination: Hashable {
-        case roomDetails(room: StorageRoom)
+        case modulesList(_ modules: [Module])
+        case roomDetails(room: StorageRoom, targetModule: Module?)
     }
 
-    var path: NavigationPath = .init()
+    var path: [Destination] = []
 
     func navigate(to destination: Destination) {
         path.append(destination)
     }
 
+    func back() {
+        _ = path.popLast()
+    }
+
+    func back<T>(returning value: T) {
+        back()
+        let destination = path.popLast()
+        
+        if case let .roomDetails(room, _) = destination,
+            let targetModule = value as? Module {
+            navigate(to: .roomDetails(room: room, targetModule: targetModule))
+
+        } else if let destination {
+            navigate(to: destination)
+        }
+    }
+
     @ViewBuilder
     func view(for destination: Destination) -> some View {
         switch destination {
-        case .roomDetails(let room):
-            StorageRoomDetailsView(room: room)
+        case .modulesList(let modules):
+            ModulesListView(modules: modules, router: self)
+
+        case .roomDetails(let room, let targetModule):
+            StorageRoomDetailsView(
+                room: room,
+                targetModule: targetModule,
+                router: self
+            )
         }
     }
 }
