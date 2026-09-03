e15b3a5567ab04eee755840b6c2c8398ec633f33
Victor Barrios <victor@beiningbogen.no>
2026-09-03 23:54:22 +0200

    [feat] Display an empty message when a module has no items

--- Modules/FeatureModules/StorageRoomFeature/Sources/StoragePresentation/ModuleDetails/ModuleDetailsView.swift
+++ Modules/FeatureModules/StorageRoomFeature/Sources/StoragePresentation/ModuleDetails/ModuleDetailsView.swift
@@ -8,19 +8,39 @@ struct ModuleDetailsView: View {
         VStack(spacing: 10) {
             ModuleStatsView(module: module)
 
-            List {
-                Section {
-                    ForEach(module.items, id: \.id) {
-                        ItemCellView(item: $0)
-                    }
-                } header: {
-                    Text("Items")
-                        .monospaced()
-                }
-                .padding(.top, 10)
+            if module.items.count <= 0 {
+                emptyList
+            } else {
+                itemsList
             }
         }
         .navigationTitle(module.label)
+    }
+
+    private var itemsList: some View {
+        List {
+            Section {
+                ForEach(module.items, id: \.id) {
+                    ItemCellView(item: $0)
+                }
+            } header: {
+                Text("Items")
+                    .monospaced()
+            }
+            .padding(.top, 10)
+        }
+    }
+
+    private var emptyList: some View {
+        VStack(spacing: 10) {
+            Image(systemName: "tray")
+                .font(.system(size: 40))
+
+            Text("Empty module")
+                .font(.title2.monospaced())
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(uiColor: .secondarySystemBackground))
     }
 }
 
